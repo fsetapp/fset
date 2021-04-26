99216c34c4c389c131fcabae20c4d84c1ae3eaea
50kudos <50th.zaint@gmail.com>
2021-04-26 15:32:27 +0700

    Move sch action buttons down closed to fmodelTree
    The previous position will be some general navs and search box

--- lib/fset_web/views/icon_view.ex
+++ lib/fset_web/views/icon_view.ex
@@ -1,3 +1,5 @@
+# sources https://heroicons.com
+
 defmodule FsetWeb.IconView do
   use FsetWeb, :view
 
@@ -97,10 +99,19 @@ defmodule FsetWeb.IconView do
     svg_tag("add", path, opts)
   end
 
+  def settings(opts \\ []) do
+    path = ~E"""
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+    """
+
+    svg_tag("settings", path, opts)
+  end
+
   defp svg_tag(title, path, opts) do
     ~E"""
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="<%= opts[:viewbox] || '0 0 24 24' %>" class="<%= opts[:class] || 'w-6 h-6' %>" stroke="currentColor">
-      <title><%= title %></title>
+      <title><%= opts[:title] || title %></title>
       <%= path %>
     </svg>
     """
