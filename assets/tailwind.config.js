49779bfbbd50f49a8062a132c673711a6fa09e13
50kudos <50th.zaint@gmail.com>
2021-07-01 18:09:24 +0700

    Bump Vite and Tailwindcss versions

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,13 +1,8 @@
 module.exports = {
-  purge: {
-    enabled: process.env.NODE_ENV == "production",
-    content: [
-      "../lib/fset_web/templates/**/*.{leex,eex}",
-      "../lib/fset_web/views/*.ex",
-      "./js/**/*.js"
-    ],
-    options: {
-      safelist: []
-    }
-  }
+  mode: "jit",
+  purge: [
+    "../lib/fset_web/templates/**/*.{leex,eex}",
+    "../lib/fset_web/views/*.ex",
+    "./js/**/*.js"
+  ]
 }
